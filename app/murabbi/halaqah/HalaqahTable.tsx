c09efc9f63e95de671cf85b651a317af7cc3e1c0
Daffa Radifanka <daffa.radifanka@dretail.id>
2025-02-23 23:18:13 +0700

    responsive ui

--- app/murabbi/halaqah/HalaqahTable.tsx
+++ app/murabbi/halaqah/HalaqahTable.tsx
@@ -1,4 +1,4 @@
-import { Table, Thead, Tbody, Tr, Th, Td, IconButton } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td, IconButton, Box } from "@chakra-ui/react";
 import { FiEdit, FiTrash } from "react-icons/fi";
 import { Halaqah } from "@/app/murabbi/halaqah/types";
 
@@ -10,40 +10,42 @@ type Props = {
 
 export default function HalaqahTable({ halaqahList, onEdit, onDelete }: Props) {
   return (
-    <Table variant="simple">
-      <Thead>
-        <Tr>
-          <Th>Nama Halaqah</Th>
-          <Th>Jumlah Anggota</Th> {/* Tambahkan kolom baru */}
-          <Th>Kode Halaqah</Th>
-          <Th>Aksi</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {halaqahList.map((halaqah) => (
-          <Tr key={halaqah.id}>
-            <Td>{halaqah.nama}</Td>
-            <Td>{halaqah.jumlahAnggota}</Td> {/* Tampilkan jumlah anggota */}
-            <Td>{halaqah.kode}</Td>
-            <Td>
-              <IconButton
-                aria-label="Edit"
-                icon={<FiEdit />}
-                size="sm"
-                onClick={() => onEdit(halaqah)}
-                mr={2}
-              />
-              <IconButton
-                aria-label="Hapus"
-                icon={<FiTrash />}
-                size="sm"
-                colorScheme="red"
-                onClick={() => onDelete(halaqah.id)}
-              />
-            </Td>
+    <Box overflowX="auto" w="full">
+      <Table variant="simple" size="sm">
+        <Thead>
+          <Tr>
+            <Th>Nama Halaqah</Th>
+            <Th display={{ base: "none", md: "table-cell" }}>Jumlah Anggota</Th>
+            <Th>Kode Halaqah</Th>
+            <Th>Aksi</Th>
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
+        </Thead>
+        <Tbody>
+          {halaqahList.map((halaqah) => (
+            <Tr key={halaqah.id}>
+              <Td whiteSpace="nowrap">{halaqah.nama}</Td>
+              <Td display={{ base: "none", md: "table-cell" }}>{halaqah.jumlahAnggota}</Td>
+              <Td whiteSpace="nowrap">{halaqah.kode}</Td>
+              <Td>
+                <IconButton
+                  aria-label="Edit"
+                  icon={<FiEdit />}
+                  size="sm"
+                  onClick={() => onEdit(halaqah)}
+                  mr={2}
+                />
+                <IconButton
+                  aria-label="Hapus"
+                  icon={<FiTrash />}
+                  size="sm"
+                  colorScheme="red"
+                  onClick={() => onDelete(halaqah.id)}
+                />
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Box>
   );
 }
