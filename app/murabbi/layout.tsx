41bacc8f2d7c96129bf32c1c9e914bd13bd943c6
Daffa Radifanka <daffa.radifanka@dretail.id>
2025-04-10 16:32:35 +0700

    add new features amalan for murabbi

--- app/murabbi/layout.tsx
+++ app/murabbi/layout.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { Box } from "@chakra-ui/react";
+import { Box, IconButton } from "@chakra-ui/react";
 import Header from "@/components/Header";
 import BottomNav from "@/components/BottomNav";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { FiPlus } from "react-icons/fi";
+
 
 export default function MurabbiLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -15,12 +18,33 @@ export default function MurabbiLayout({ children }: { children: React.ReactNode 
     ? "Profile"
     : "Dashboard Murabbi";
 
+    const showFab =
+  pathname.startsWith("/murabbi/amalan") &&
+  !pathname.endsWith("/create") &&
+  !/^\/murabbi\/amalan\/[0-9a-fA-F-]{36}$/.test(pathname);
+
   return (
     <Box minH="100vh" pt="0px" pb="120px">
     {/* <WarningModal/> */}
       {/* <Header title={title} /> */}
       {children}
       <BottomNav />
+
+      {/* Floating Action Button */}
+      {showFab && (
+        <Box position="fixed" bottom="90px" right="24px" zIndex={1000}>
+          <Link href="/murabbi/amalan/create">
+            <IconButton
+              icon={<FiPlus />}
+              aria-label="Tambah Amalan"
+              colorScheme="teal"
+              size="lg"
+              isRound
+              boxShadow="lg"
+            />
+          </Link>
+        </Box>
+      )}
     </Box>
   );
 }
--- lib/api.ts
+++ lib/api.ts
@@ -4,9 +4,15 @@ export const api = {
   register: `${API_URL}/auth/register`,
   login: `${API_URL}/auth/login`,
   getProfile: `${API_URL}/profile`,
+
   getAmalan: `${API_URL}/amalan/get`,
+  addAmalan :`${API_URL}/amalan/add`,
+  getAmalanForMurabbi: `${API_URL}/amalan/get-amalan-for-murabbi`,
   postAmalanHarian: `${API_URL}/amalan/amalan-harian`,
   getAmalanHarian: `${API_URL}/amalan/harian`,
+  createAmalan: `${API_URL}/amalan/harian`,
+  updateStatusAmalan: `${API_URL}/amalan/update-status`,
+
   dashboardMurrabi: `${API_URL}/dashboard/murabbi`,
   dashboardTholib: `${API_URL}/dashboard/tholib`,
   halaqahMurabbi: `${API_URL}/halaqah/murabbi`,
